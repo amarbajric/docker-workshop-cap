87773af679bac9a9b93e20183c82e6a1702a8ab8
Amar Bajric <e062342@buero.oebb.at>
2024-02-06 13:06:11 +0100

    fix cmd for 01 python file

--- practical-examples/01-python/01-python-file/cmd.sh
+++ practical-examples/01-python/01-python-file/cmd.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v /mnt/c/Users/abajric/OneDrive - Capgemini/Documents/docker-workshop/practical-examples/01-python/01-python-file:/src python:3 python /src/hello.py
+docker run --rm -it -v $(pwd):/src python:3 python /src/hello.py
